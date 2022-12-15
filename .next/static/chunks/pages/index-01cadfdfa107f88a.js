d63ce859e3325f9c09621fdc1e2e754b4e2d2073
Paul Pacurar <ppaul.dev@gmail.com>
2022-12-15 18:07:44 +0200

    empty project

---
+++ .next/static/chunks/pages/index-01cadfdfa107f88a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{5557:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return _(4369)}])},4369:function(n,u,_){"use strict";_.r(u);var e=_(5893);u.default=()=>(0,e.jsx)("p1",{children:"Hello"})}},function(n){n.O(0,[774,888,179],function(){return n(n.s=5557)}),_N_E=n.O()}]);
